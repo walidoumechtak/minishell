ec39e397ad5c1c0164106c3562623751e5958b15
Walid Oumechtak <woumecht@e1r8p9.1337.ma>
2023-02-23 15:55:28 +0100

    update

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: woumecht <woumecht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:48:56 by woumecht          #+#    #+#             */
-/*   Updated: 2023/02/23 15:33:56 by woumecht         ###   ########.fr       */
+/*   Updated: 2023/02/23 15:49:53 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int main()
     while (1)
     {
         str = readline(RED"Minishell"NONE GREEN"-$ "NONE);
+        add_hisory(str); // ==> add to cammand history
         printf("%s \n", str);
     }
     free(str);
