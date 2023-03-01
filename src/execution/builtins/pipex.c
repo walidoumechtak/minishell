fa5f54a0818b79458d2c24a68caa067558a13db6
Hamza benfaddoul <benfaddoul01@gmail.com>
2023-03-01 13:50:33 +0100

    update pipex.c

--- src/execution/builtins/pipex.c
+++ src/execution/builtins/pipex.c
@@ -9,36 +9,30 @@ int	main(int ac, char **av)
 
 	i = 1;
 	av[1] = "/bin/ls";
-	av[3] = NULL;
-	int pid0 = fork();
-	if (pid0 == 0)
-	{
-		while (av[i + 1] || av[i + 1][0])
-		{
+	av[2] = "/bin/ls";
+	av[3] = "/bin/ls";
+	av[4] = "/usr/bin/echo";
+	av[5] = NULL;
 
-			
-			pipe(fd); 
-			int pid1 = fork();
-			if (pid1 < 0)
-				return (-1);
-			if (pid1 == 0)
-			{
-				dup2(fd[1], STDOUT_FILENO);
-				close(fd[0]);
-				close(fd[1]);
-				execve(av[i], NULL, NULL);
-			}
-			wait(NULL);
-			dup2(fd[0], STDIN_FILENO);
+	pipe(fd);
+	while (av[i + 1])
+	{
+		i++;
+		int pid1 = fork();
+		if (pid1 == 0)
+		{
+			char *args[] = {av[i], NULL};
+			dup2(fd[1], STDOUT_FILENO);
 			close(fd[0]);
 			close(fd[1]);
-			i++;
+			execve(av[i], args, NULL);
 		}
+		wait(NULL);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
 	}
-	wait(NULL);
-	close(fd[0]);
-	close(fd[1]);
-	execve(av[i], NULL, NULL);
-
+	char *args[] = {av[i], "hamza", NULL};
+	execve(av[i], args, NULL);
 	return (0);
 }
