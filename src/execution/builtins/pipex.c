77b81555a54e701921abdb685c3e1f2cb0bff5e0
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-03-01 18:48:41 +0100

    update pip

--- src/execution/builtins/pipex.c
+++ src/execution/builtins/pipex.c
@@ -1,38 +1,41 @@
 
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
 	int	fd[2];
 	int i;
 
-	i = 1;
+	i = 0;
 	av[1] = "/bin/ls";
-	av[2] = "/bin/ls";
-	av[3] = "/bin/ls";
-	av[4] = "/usr/bin/echo";
+	av[2] = "/usr/bin/wc";
+	av[3] = "/usr/bin/wc";
+	av[4] = "/bin/ls";
 	av[5] = NULL;
 
-	pipe(fd);
-	while (av[i + 1])
+	dup2(0, 0);
+	while (av[i])
 	{
-		i++;
+		char *args[] = {av[i], NULL};
+		pipe(fd);
 		int pid1 = fork();
 		if (pid1 == 0)
 		{
-			char *args[] = {av[i], NULL};
-			dup2(fd[1], STDOUT_FILENO);
-			close(fd[0]);
+			if (av [i + 1] != NULL)
+				dup2(fd[1], STDOUT_FILENO);
+			else
+				dup2(1, 1);
 			close(fd[1]);
+			close(fd[0]);
 			execve(av[i], args, NULL);
 		}
-		wait(NULL);
 		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
 		close(fd[1]);
+		close(fd[0]);
+		i++;
 	}
-	char *args[] = {av[i], "hamza", NULL};
-	execve(av[i], args, NULL);
+	wait(NULL);
 	return (0);
 }
