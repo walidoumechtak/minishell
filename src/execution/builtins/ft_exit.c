13b8c71fbf620033f488e09dcaa0dff3a4cd2804
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-03-11 12:26:07 +0100

    add ft_exit.c

---
+++ src/execution/builtins/ft_exit.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/11 08:28:51 by hbenfadd          #+#    #+#             */
+/*   Updated: 2023/03/11 11:41:32 by hbenfadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_exit(char **args)
+{
+	if (ft_isalpha((int)**args))
+	{
+		ft_putstr_fd ("exit\nminishell: exit: ", 2);
+		ft_putstr_fd(*args, 2);
+		ft_putstr_fd(": numeric argument required\n", 2);
+		exit(255);
+	}
+	else if (args[1])
+	{
+		ft_putstr_fd("exit\nminishell: exit: too many argument\n", 2);
+		return (1);
+	}
+	exit(ft_atoi(*args));
+}
+
+
