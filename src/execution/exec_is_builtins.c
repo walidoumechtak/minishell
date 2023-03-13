3a44bfbebd141fe179f37cb4d6f4838fb19fec7f
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-03-13 13:43:07 +0100

    updtae exec_isbuiltins.c

--- src/execution/exec_is_builtins.c
+++ src/execution/exec_is_builtins.c
@@ -1,25 +1,23 @@
 #include "minishell.h"
 
-int	exec_is_builtins(t_minishell *shell, t_list *env)
+int	exec_is_builtins(t_minishell *shell, char **args, t_list *env)
 {
 	size_t	len;
 	char	*cmd;
-	char	**args;
 
-	cmd = *((t_cmd *)shell->list_cmd->content)->cmd;
-	args = ++((t_cmd *)shell->list_cmd->content)->cmd;
+	cmd = *args;
 	len = ft_strlen(cmd);
 	if (len == 4 && ft_strnstr(cmd, "echo", len))
-		return (ft_echo(args));
+		exit (ft_echo(args));
 	else if (len == 5 && ft_strnstr(cmd, "unset", len))
-		return (ft_unset(shell, args));
-		else if (len == 2 && ft_strnstr(cmd, "cd", len))
-		return (ft_cd(args));
+		exit (ft_unset(shell, args));
+	else if (len == 2 && ft_strnstr(cmd, "cd", len))
+		exit (ft_cd(args));
 	else if (len == 3 && ft_strnstr(cmd, "env", len))
-		return (ft_env(env));
+		exit (ft_env(env));
 	else if (len == 4 && ft_strnstr(cmd, "exit", len))
-		return (ft_exit(args));
+		exit (ft_exit(args));
 	// else if (len == 6 && ft_strnstr(cmd, "export", len))
 	// 	return (ft_export(args));
-	return (-1);
+	return (1);
 }
