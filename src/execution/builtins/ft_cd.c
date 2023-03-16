04b6afffd492a9311a8302bc04f09f2aee840369
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-03-16 14:20:55 +0100

    update ft_cd

--- src/execution/builtins/ft_cd.c
+++ src/execution/builtins/ft_cd.c
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hamza <hamza@student.42.fr>                +#+  +:+       +#+        */
+/*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 14:42:51 by hbenfadd          #+#    #+#             */
-/*   Updated: 2023/03/16 08:20:12 by hamza            ###   ########.fr       */
+/*   Updated: 2023/03/16 12:36:04 by hbenfadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_list *get_avr(t_minishell *shell, char *var)
+static void	get_avr(t_minishell *shell, char *var)
 {
 	t_list	*tmp;
 	char	*env_var;
+	char	*buff;
 
+	buff = NULL;
 	tmp = shell->env;
 	while (tmp)
 	{
@@ -24,32 +26,23 @@ static t_list *get_avr(t_minishell *shell, char *var)
 		if (ft_strnstr(env_var, var, ft_strlen(var))
 			&& ft_strlen(var) == ft_strlen(env_var))
 		{
-			return (tmp);
+			free(((t_env *)tmp->content)->env_value);
+			((t_env *)tmp->content)->env_value = getcwd(buff, 0);
+			return ;
 		}
 		tmp = tmp->next;
 	}
-	return (NULL);
 }
 
 int	ft_cd(t_minishell *shell, char **args)
 {
-	t_list	*tmp;
-	t_list	*pwd;
-	t_list	*oldpwd;
-	char	*buff;
-
-	buff = NULL;
+	get_avr(shell, "OLDPWD");
 	if (chdir(*args) == -1)
 	{
-		ft_putstr_fd("minishell: cd: ", 2);
+		ft_putstr_fd("minishell: ", 2);
 		perror(*args);
 		exit(EXIT_FAILURE);
 	}
-	oldpwd = get_avr(shell, "OLDPWD");
-	pwd = get_avr(shell, "PWD");
-	free(((t_env *)oldpwd->content)->env_value);
-	((t_env *)oldpwd->content)->env_value = ((t_env *)pwd->content)->env_value;
-	tmp = pwd;
-  	((t_env *)pwd->content)->env_value = getcwd(buff, 0);
+	get_avr(shell, "PWD");
 	return (EXIT_SUCCESS);
 }
