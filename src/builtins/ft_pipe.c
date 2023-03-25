b1bff2bc9156764f6f50f0e1859f427ee8bf5a82
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-03-25 11:58:30 +0000

    handle signale and close fd

--- src/builtins/ft_pipe.c
+++ src/builtins/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 10:30:59 by hbenfadd          #+#    #+#             */
-/*   Updated: 2023/03/23 07:05:06 by hbenfadd         ###   ########.fr       */
+/*   Updated: 2023/03/25 11:59:18 by hbenfadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void	exec_cmd(t_minishell *shell, t_cmd	*s_cmd, t_list *next)
 	pipe(fd);
 	if (fork() == 0)
 	{
-		if (s_cmd->fd_in == -1 || s_cmd->fd_in == -1)
+		if (s_cmd->fd_in == -1 || s_cmd->fd_out == -1)
 			exit(1);
 		if (next != NULL && s_cmd->fd_out == STDOUT_FILENO)
 			dup2(fd[1], STDOUT_FILENO);
@@ -35,7 +35,8 @@ static void	exec_cmd(t_minishell *shell, t_cmd	*s_cmd, t_list *next)
 		cmd = check_cmd(*s_cmd->cmd, shell->env);
 		execve(cmd, s_cmd->cmd, convert_list_env(shell->env));
 	}
-	dup2(fd[0], STDIN_FILENO);
+	if (next)
+		dup2(fd[0], STDIN_FILENO);
 	close(fd[1]);
 	close(fd[0]);
 }
@@ -49,6 +50,8 @@ void	ft_pipe(t_minishell *shell, t_list *cmd)
 		dup2(((t_cmd *)(cmd->content))->fd_in, STDIN_FILENO);
 	while (cmd)
 	{
+		if (((t_cmd *)cmd->content)->fd_in == 1)
+			dup2(stdin, STDIN_FILENO);
 		exec_cmd(shell, cmd->content, cmd->next);
 		cmd = cmd->next;
 	}
@@ -56,4 +59,5 @@ void	ft_pipe(t_minishell *shell, t_list *cmd)
 		;
 	shell->exit_state = WEXITSTATUS(shell->exit_state);
 	dup2(stdin, STDIN_FILENO);
+	close(stdin);
 }
