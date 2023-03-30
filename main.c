2e1515a1ccba91d5a60f834088435e76ae06b841
Walid Oumechtak <woumecht@e1r2p12.1337.ma>
2023-03-30 11:30:20 +0000

    done with norminette

--- main.c
+++ main.c
@@ -6,26 +6,55 @@
 /*   By: woumecht <woumecht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:48:56 by woumecht          #+#    #+#             */
-/*   Updated: 2023/03/29 16:54:21 by woumecht         ###   ########.fr       */
+/*   Updated: 2023/03/30 11:29:57 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	main_support(t_minishell *ptr)
+{
+	int	state;
+
+	if (g_free_flag == 1)
+	{
+		g_free_flag = 0;
+		ptr->exit_state = 1;
+	}
+	init_struct(ptr);
+	state = parsing(ptr);
+	if (state != 0)
+	{
+		all_errors_parsing(ptr, state);
+		free(ptr->str);
+		free_spilte(ptr->splited_pipe);
+		return (1);
+	}
+	else
+		ptr->exit_state = 0;
+	ft_exec(ptr);
+	end_of_program(ptr);
+	return (0);
+}
+
+void	ptr_failed(t_minishell *ptr)
+{
+	g_free_flag = 0;
+	if (!ptr)
+	{
+		ft_putstr_fd("Error: memory allocation\n", 2);
+		exit(1);
+	}
+}
+
 int	main(int ac, char **av, char **env)
 {
 	t_minishell	*ptr;
-	int			state;
 
 	(void)av;
 	(void)ac;
 	ptr = malloc(sizeof(t_minishell));
-	if (!ptr)
-	{
-		ft_putstr_fd("Error: memory allocation\n", 2);
-		exit(404);
-	}
-	g_free_flag = 0;
+	ptr_failed(ptr);
 	ptr->env = build_env_list(env);
 	ptr->exit_state = 0;
 	signal(SIGQUIT, SIG_IGN);
@@ -41,24 +70,8 @@ int	main(int ac, char **av, char **env)
 			continue ;
 		}
 		add_history(ptr->str);
-		if (g_free_flag == 1)
-		{
-			g_free_flag = 0;
-			ptr->exit_state = 1;
-		}
-		init_struct(ptr);
-		state = parsing(ptr);
-		if (state != 0)
-		{
-			all_errors_parsing(ptr, state);
-			free(ptr->str);
-			free_spilte(ptr->splited_pipe);
+		if (main_support(ptr) != 0)
 			continue ;
-		}
-		else
-			ptr->exit_state = 0;
-		ft_exec(ptr);
-		end_of_program(ptr);
 	}
 	free(ptr);
 }
--- src/parsing/fill_with.c
+++ src/parsing/fill_with.c
@@ -6,72 +6,42 @@
 /*   By: woumecht <woumecht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 11:26:48 by woumecht          #+#    #+#             */
-/*   Updated: 2023/03/29 18:25:56 by woumecht         ###   ########.fr       */
+/*   Updated: 2023/03/30 11:23:10 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    fill_with(char *str,char old, char c)
+void	singl_case_qoutes(char *str, int *i, char old, char c)
 {
-    int i;
-
-    if (!str)
-        return ;
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == '\"' && str[i + 1] != '\0')
-        {
-            i++;
-            while (str[i] && str[i] != '\"')
-            {
-                if (str[i] == old)
-                    str[i] = c;
-                i++;
-            }
-        }
-        else if (str[i] == '\'' && str[i + 1] != '\0')
-        {
-            i++;
-            while (str[i] && str[i] != '\'')
-            {
-                if (str[i] == old)
-                    str[i] = c;
-                i++;
-            }
-        }
-        i++;
-    }
+	(*i)++;
+	while (str[*i] && str[*i] != '\'')
+	{
+		if (str[*i] == old)
+			str[*i] = c;
+		(*i)++;
+	}
 }
 
-// void	fill_with(char *str, char old, char c)
-// {
-// 	int	i;
+void	fill_with(char *str, char old, char c)
+{
+	int	i;
 
-// 	i = 0;
-// 	if (!str)
-//         return ;
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '\"' && str[i + 1] != '\0')
-// 		{
-// 			i++;
-// 			while (str[i] && str[i++] != '\"')
-// 			{
-// 				if (str[i] == old)
-// 					str[i] = c;
-// 			}
-// 		}
-// 		else if (str[i] == '\'' && str[i + 1] != '\0')
-// 		{
-// 			i++;
-// 			while (str[i] && str[i++] != '\'')
-// 			{
-// 				if (str[i] == old)
-// 					str[i] = c;
-// 			}
-// 		}
-// 		i++;
-// 	}
-// }
+	i = 0;
+	while (str && str[i])
+	{
+		if (str[i] == '\"' && str[i + 1] != '\0')
+		{
+			i++;
+			while (str[i] && str[i] != '\"')
+			{
+				if (str[i] == old)
+					str[i] = c;
+				i++;
+			}
+		}
+		else if (str[i] == '\'' && str[i + 1] != '\0')
+			singl_case_qoutes(str, &i, old, c);
+		i++;
+	}
+}
